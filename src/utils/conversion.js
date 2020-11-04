351605f9aaf34ca207fb81651679539546942ab4
Auroiah Morgan <amwebdev86@outlook.com>
2020-11-03 23:31:21 -0500

    Add Bootstrap and documentation

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -1,4 +1,10 @@
- function truncateString(str, length, ending) {
+/**
+ * Truncates the length of a string setting desired length and desired ending to append.
+ * @param {string} str 
+ * @param {int} length 
+ * @param {string} ending 
+ */ 
+function truncateString(str, length, ending) {
   if (length === null) {
     length = 50
   }
@@ -11,10 +17,17 @@
     return str
   }
 }
-
+/**
+ * converts Kelvin to Fahrenheit returning and appending the degree symbol 
+ * @param {int} k 
+ */
 function convertKelvinToFah(k) {
   return (((k - 273.15) * 9) / 5 + 32).toFixed(2) + " \xB0F"
 }
+/**
+ * converts Kelvin to Celsius to a fixed decimal of 2 'x.xx' and returning and appending the degree symbol
+ * @param {double} k 
+ */
 function convertKelvinToCelsius(k) {
   return (k - 273.15).toFixed(2) + "\xB0C"
 }
